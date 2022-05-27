42ad60c5dc80a35855dcc8fde02682410642eeea
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-05-28 02:11:03 +0530

    fix index file not found 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 import MongoStore = require("connect-mongo");
 import express = require("express");
+import * as path from "path";
 import session = require("express-session");
 import cors = require("cors");
 import cookieParser = require("cookie-parser");
 import { connectToDb } from "./utils/connect";
 import router from "./routes";
+import { Response, Request } from "express";
 const app = express();
 
 connectToDb(process.env.MONGO_URL as string);
@@ -43,6 +45,15 @@ app.use(
 );
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  router.use(express.static("personalProjectFE/build"));
+  router.get("*", (req: Request, res: Response) => {
+    res.sendFile(
+      path.join(__dirname, "../", "personalProjectFE", "build", "index.html")
+    );
+  });
+}
 
 app.use(router);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,10 @@
 import authRoutes from "./routes.auth";
 import userRoutes from "./routes.user";
-import * as path from "path";
 
 import express = require("express");
 import { Request, Response, Router } from "express";
 
 const router = Router();
-
-if (process.env.NODE_ENV === "production") {
-  router.use(express.static("personalProjectFE/build"));
-  router.get("*", (req: Request, res: Response) => {
-    res.sendFile("../../personalProjectFE/build/index.html");
-  });
-}
 
 router.use(authRoutes);
 router.use(userRoutes);
